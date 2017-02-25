0b57d3fb83b7087334f59fc07be0721e01f9371a
Terrance Lee <terranceleeii13@gmail.com>
2017-02-25 13:36:21 -0800

    update

--- HW4.hs
+++ HW4.hs
@@ -6,75 +6,50 @@ import Data.Function (fix)
 import KarelSyntax
 import KarelState
 
-getCard :: Robot -> Card
-getCard (_, card, _) = card
-
--- | Valuation function for Test.
-test :: Test -> World -> Robot -> Bool
-test = (Not theTest) world robot =
-not (test theTest world robot)
-
-test (Facing cardinalDir) _ (_, robotFacingDir, _)=
-cardinalDir == robotFacingDir
-
-test(Clear relativeDir) world (robotPos, robotFacingDir, _)=
-let
-absoluteDir = cardTurn relativeDir robotFacingDir
-neighboringSpace = world (neighbor absoluteDir robotPos)
-in isJust neighboringSpace
-
-test Beeper world (robotPos, _, _) +
-maybe False (> 0) (world robotPos)
-
-test Empty _ (_, _, robotBeepers) = robotBeepers > 0
-
+test (Not t)    w r = not (test t w r)
+test (Facing c) _ r = c == getFacing r
+test (Clear d)  w r = isClear (relativePos d r) w
+test (Beeper)   w r = hasBeeper (getPos r) w
+test (Empty)    _ r = isEmpty r
 
 -- | Valuation function for Stmt.
 stmt :: Stmt -> Defs -> World -> Robot -> Result
-stmt Shutdown   _ _ r = Done r
-stmt PickBeeper _ w r = let p = getPos r
-                        in if hasBeeper p w
-                              then OK (decBeeper p w) (incBag r)
-                              else Error ("No beeper to pick at: " ++ show p)
+stmt Shutdown       _ _ r = Done r
+stmt Move           _ w r = let p = relativePos Front r
+                            in if isClear p w then OK w (setPos p r)
+                                              else Error ("Blocked at: " ++ show p)
+stmt PickBeeper     _ w r = let p = getPos r
+                            in if hasBeeper p w
+                                  then OK (decBeeper p w) (incBag r)
+                                  else Error ("No beeper to pick at: " ++ show p)
+stmt PutBeeper      _ w r = let p = getPos r
+                            in if isEmpty r then Error "No beeper to put."
+                                            else OK (incBeeper p w) (decBag r)
+stmt (Turn d)       _ w r = OK w (setFacing (cardTurn d (getFacing r)) r)
+stmt (Block [])     d w r = OK w r
+stmt (Block (s:ss)) d w r = case stmt s d w r of
+                              (OK w' r') -> stmt (Block ss) d w' r'
+                              (Done r') -> Done r'
+                              (Error m) -> Error m
+stmt (If t bt be)   d w r = if test t w r then stmt bt d w r
+                                          else stmt be d w r
+stmt (Call m)       d w r = case lookup m d of
+                              (Just b) -> stmt b d w r
+                              _        -> Error ("Undefined macro: " ++ m)
+stmt (Iterate i b)  d w r = if i > 0 then case stmt b d w r of
+                                            (OK w' r') -> stmt (Iterate (i-1) b) d w' r'
+                                            (Done r') -> Done r'
+                                            (Error m) -> Error m
+                                     else OK w r
+stmt (While t b)    d w r = if test t w r then case stmt b d w r of
+                                                 (OK w' r') -> stmt (While t b) d w' r'
+                                                 (Done r') -> Done r'
+                                                 (Error m) -> Error m
+                                          else OK w r
 
-stmt PutBeeper _ world robot = 
-  let pos = getPos robot in
-  let newWorld = incBeeper pos world in
-  let newRobot = decBag robot
-  in if isEmpty robot
-     then Error ("No beeper to put at: " ++ show pos)
-	 else OK newWorld newRobot
-	 
-stmt (Turn dir)_world robot =
-  let 
-    newCard = cardTurn dir (getCard robot)
-	newRobot = setFacing newCard robot
-  in OK world newRobot
-  
-stmt (Call macro) defs world robot =
-  case lookup macro defs of
-   Just innerStmt -> stmt innerStmt defs world robot
-   _ -> Error ("Unknown macro called: " ++ show macro)   
-   
-stmt (Iterate i innerStmt) defs world robot =
-  if i = 0
-     then OK world robot
-	 else case stmt innerStmt defs world robot of
-	   OK newWorld newRobot ->
-	      stmt (Iterate (i-1) innerStmt) defs newWorld newRobot
-	   result -> result
-	   
-stmt (If cond thenStmt elseStmt) defs world robot =
-  if test cond world robot
-     then stmt thenStmt defs world robot
-	 else stmt elseStmt defs world robot
-	 
-stmt (While cond body) defs world robot =
-  if test cond world robot
-     then case stmt body defs world robot of
-	   (OK newWorld newRobot) -> stmt (While cond body) defs newWorld newRobot
-	   result -> result
-	 else OK world robot
+-- | Run a Karel program.
+prog :: Prog -> World -> Robot -> Result
+prog (m,s) w r = stmt s m w r
 	 
 stmt (Block [])_world robot = OK world robot
 stmt (Block (statement:statements)) defs world robot =
